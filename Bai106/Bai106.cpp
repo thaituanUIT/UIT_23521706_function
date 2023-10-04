b0cbaf12df628c4dcb4134b2e5d3859db2106c92
qhuongneh <145864905+qhuongneh@users.noreply.github.com>
2023-10-04 14:23:14 +0700

    quynh huong

---
+++ Bai106/Bai106.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float sin();
+
+int main()
+{
+	float x;
+	cout << "Gia tri sin ";
+	cin >> x;
+
+    cout << sin(x);
+	return 0;
+}
+
+float sin()
+{
+	double s, m = 1, e = 1, i = 4, x, t;
+	cin >> x;
+	float dau = -1;
+	
+	double xx = (x * 3.14) / 180;
+	s = xx;
+	t = xx; 
+	e = xx;
+    float epsilon = pow(10,-6);
+
+	while (e >= pow(10, -6)) 
+	{
+		t = t * xx * xx;
+		m = m * (i - 1) * i;
+		e = t / m;
+		s = s + e * dau;
+		dau = -dau;
+		i = i + 2;
+	}
+
+	return s;
+}
