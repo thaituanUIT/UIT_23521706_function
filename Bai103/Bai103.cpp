c5704e06515ba7179b92e82b6564b1d93271db2e
qhuongneh <145864905+qhuongneh@users.noreply.github.com>
2023-10-04 13:43:50 +0700

    Quynh Huong

---
+++ Bai103/Bai103.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float Tinh();
+
+int main()
+{
+    cout << "Ket qua";
+    cout << Tinh();
+    return 0;
+}
+
+float Tinh()
+{
+    float s = 0;
+    float e = 1;
+    int i = 1;
+    float epsilon = pow(10,-6);
+    while ( e >= epsilon)
+    {
+        e = (float) 1/i;
+        s = s + e;
+        i = i + 2;
+    }
+
+    return s;
+}
